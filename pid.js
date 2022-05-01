d35ad92b24187e5c6cabcecb0b81526dafe4b73d
@cloudymedic <saeed_gholamrezaee@yahoo.com>
2022-05-01 22:40:36 +0430

    delete main file .js

--- pid.js
+++ pid.js
@@ -1,3 +1,4 @@
+var str01 = "first token set on macosx use on github to collaborate";
 console.log('write pid on the project');
 console.log('second code on origin project two');
-
+console.log('we string is : %s', str01);
